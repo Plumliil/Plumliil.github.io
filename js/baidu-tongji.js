5bc0c85498626be145475bbbe43fa636bdcd3a2a
plumliil <476453574@qq.com>
2022-03-29 11:03:00 +0800

    Site updated: 2022-03-29 11:02:59

--- js/baidu-tongji.js
+++ js/baidu-tongji.js
@@ -1,7 +1,16 @@
+// var _hmt = _hmt || [];
+// (function() {
+//   var hm = document.createElement("script");
+//   hm.src = "https://hm.baidu.com/hm.js?";
+//   var s = document.getElementsByTagName("script")[0]; 
+//   s.parentNode.insertBefore(hm, s);
+// })();
+// <script>
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?";
+  hm.src = "https://hm.baidu.com/hm.js?3e49dc29efbf3523935a9671d640bcd8";
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
+// </script>
